7a5b3fa8a6e35d776db139af8e26e10c8cf46c10
Kevin <kevin@kevinhq.com>
2015-02-09 13:42:02 +0700

    Initialize states

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150209053553) do
+ActiveRecord::Schema.define(version: 20150209062415) do
 
   create_table "campuses", force: true do |t|
     t.string   "name"
@@ -38,6 +38,15 @@ ActiveRecord::Schema.define(version: 20150209053553) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "states", force: true do |t|
+    t.string   "name"
+    t.integer  "country_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "states", ["country_id"], name: "index_states_on_country_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
