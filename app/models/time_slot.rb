94a5324c668db399ec126624432fc2bc36519393
Kevin <kevin@kevinhq.com>
2015-02-17 11:08:09 +0700

    Add validation: start_time should be before end_time

--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -6,10 +6,15 @@ class TimeSlot < ActiveRecord::Base
   validates :campus, :presence => true
   validates :start_time, :presence => true
   validates :end_time, :presence => true
+  validate :start_time_before_end_time
 
   before_save :generate_name
 
   def generate_name
   	self.name = "#{self.start_time.strftime('%H')}:#{self.start_time.strftime('%M')} – #{self.end_time.strftime('%H')}:#{self.end_time.strftime('%M')}"
+  end
+
+  def start_time_before_end_time
+    errors.add(:start_time,"Hora de inicio debe estar antes de la hora de finalización") if start_time > end_time
   end
 end
