481f790b44e52bfd77edeabd0fff35b27fd8a53d
Kevin <kevin@kevinhq.com>
2015-02-27 13:50:34 +0700

    Users Home Page

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,4 +1,8 @@
 class VisitorsController < ApplicationController
 	def index
+		if current_user.active_role.present?
+			role_id = Role.where(:name=>current_user.active_role).try(:first).id
+			@notifications = Notification.where(:role_id=>role_id)
+		end
 	end
 end
