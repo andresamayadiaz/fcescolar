0048a07adc35290c3b870f41ee406a6183488031
Kevin <kevin@kevinhq.com>
2015-02-28 13:54:35 +0700

    Change the order of the notifications, latest at the top

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,7 +2,7 @@ class VisitorsController < ApplicationController
 	def index
 		if current_user.active_role.present?
 			role_id = Role.where(:name=>current_user.active_role).try(:first).id
-			@notifications = Notification.where(:role_id=>role_id)
+			@notifications = Notification.where(:role_id=>role_id).order('created_at desc')
 		end
 	end
 end
