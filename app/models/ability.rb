5aa0eea47eb85df3dfd7368277647c25f7c401b5
Kevin <kevin@kevinhq.com>
2015-03-13 16:24:34 +0700

    Update ability for which role can search person

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -12,11 +12,17 @@ class Ability
         can [:create, :read, :update], Notification, :user_id=>user.id
         can [:create, :read, :search], Person
     when 'finance'
-        can [:create, :read, :update, :search], Person
+        can [:create, :read, :update], Person
     when 'franchise_director'
         can [:create, :read, :update, :search], Person
     when 'salesman'
-        can [:create, :read, :update, :search], Person
+        can [:search], Person
+    when 'academic_coordinator'
+        can [:search], Person
+    when 'teacher'
+        can [:search], Person
+    when 'support_executive'
+        can [:search], Person
     end
     #inni
     # The first argument to `can` is the action you are giving the user 
