7f6fb5c8f583fa8804e8294dd4b5cb8602ae13ab
Kevin <kevin@kevinhq.com>
2015-03-19 12:07:00 +0700

    Remove select box for user with one role only

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,5 +1,6 @@
 class VisitorsController < ApplicationController
 	def index
+		current_user.update_attribute(:active_role, current_user.roles.first.name) if current_user.roles.present? and current_user.roles.length==1
 		if current_user.active_role.present?
 			role_id = Role.where(:name=>current_user.active_role).try(:first).id
 			@notifications = Notification.where(:role_id=>role_id).order('created_at desc')
