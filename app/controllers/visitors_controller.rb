29003e9fe9099cb1fe5f7063fe3f95773271dc94
Kevin <kevin@kevinhq.com>
2015-03-20 15:35:07 +0700

    Apply role status active/inactive filter to whole system

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -5,5 +5,6 @@ class VisitorsController < ApplicationController
 			role_id = Role.where(:name=>current_user.active_role).try(:first).id
 			@notifications = Notification.where(:role_id=>role_id).order('created_at desc')
 		end
+		@enabled_roles = current_user.users_roles.active
 	end
 end
