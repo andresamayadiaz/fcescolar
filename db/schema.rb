89d9ea44805dbed36979848972e43e52a1b83a94
Andres Amaya Diaz <andres.amaya.diaz@gmail.com>
2015-04-06 12:53:58 -0500

    General Catalogues View Translations Spanish

--- db/schema.rb
+++ db/schema.rb
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(version: 20150406113015) do
   create_table "careers", force: true do |t|
     t.integer  "franchise_id"
     t.string   "name"
-    t.boolean  "status",                 default: true
+    t.boolean  "status"
     t.string   "rvoe"
     t.integer  "study_level_id"
     t.string   "serie"
@@ -367,21 +367,6 @@ ActiveRecord::Schema.define(version: 20150406113015) do
   end
 
   add_index "study_levels", ["franchise_id"], name: "index_study_levels_on_franchise_id", using: :btree
-
-  create_table "study_plans", force: true do |t|
-    t.integer  "career_id"
-    t.integer  "period_id"
-    t.integer  "number_of_periods"
-    t.string   "name"
-    t.integer  "attendance_rate"
-    t.integer  "extra_opportunities"
-    t.boolean  "status",              default: true
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "study_plans", ["career_id"], name: "index_study_plans_on_career_id", using: :btree
-  add_index "study_plans", ["period_id"], name: "index_study_plans_on_period_id", using: :btree
 
   create_table "subjects", force: true do |t|
     t.integer  "franchise_id"
