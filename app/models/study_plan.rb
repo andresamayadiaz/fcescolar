4f7a97389649e0ecfe496248bffc89363f716e31
Kevin <kevin@kevinhq.com>
2015-04-10 12:54:18 +0700

    Fix problem with double nested form

--- app/models/study_plan.rb
+++ app/models/study_plan.rb
@@ -3,4 +3,6 @@ class StudyPlan < ActiveRecord::Base
   belongs_to :period
   has_many :study_plan_periods
   scope :active, -> { where(status: true) } 
+
+  accepts_nested_attributes_for :study_plan_periods, :reject_if => :all_blank, :allow_destroy => true
 end
