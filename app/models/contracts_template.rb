e94b695348b70b81466aa98663b50a418e41aeb3
Kevin <kevin@kevinhq.com>
2015-04-23 10:41:11 +0700

    Fix problem with replacing keywords

--- app/models/contracts_template.rb
+++ app/models/contracts_template.rb
@@ -4,19 +4,17 @@ class ContractsTemplate < ActiveRecord::Base
   scope :active, -> { where(status: true) }
 
   def self.replace_keywords(content,person,franchise)
-    vals = {
-      person_fullname: person.name,
-      person_fullname2: person.name,
-      person_id: person.id,
-      username: person.user.name,
-      franchise_name: franchise.name,
-      franchise_address: franchise.address,
-      date_now: Time.now.strftime('%Y-%m-%d'),
-      date_time: Time.now.strftime('%Y-%m-%d %H:%M:%S'),
-      person_street: person.person_living_address.street,
-      person_streetnumber1: person.person_living_address.num_ext
-    }
-    return content % vals
+    content.gsub! '@@person_fullname@@', person.name
+    content.gsub! '@@person_fullname2@@', person.name
+    content.gsub! '@@person_id@@', person.id.to_s
+    content.gsub! '@@username@@', person.user.name
+    content.gsub! '@@franchise_name@@', franchise.name
+    content.gsub! '@@franchise_address@@', franchise.address
+    content.gsub! '@@date_now@@', Time.now.strftime('%Y-%m-%d')
+    content.gsub! '@@date_time@@', Time.now.strftime('%Y-%m-%d %H:%M:%S')
+    content.gsub! '@@person_street@@', person.person_living_address.street
+    content.gsub! '@@person_streetnumber1@@', person.person_living_address.num_ext
+    return content
   end
 
 end
