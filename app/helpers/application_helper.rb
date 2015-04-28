4eab4baa678eb5e8b3cc27c757d35295ff704251
Kevin <kevin@kevinhq.com>
2015-04-28 18:48:43 +0700

    Actions button in progress

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 module ApplicationHelper
 
+  def dictamination_actions(status)
+    case status
+    when 'Pending' 
+			"<a href='javascript:void(0);' class='btn-sm btn-success'>Accept</a>&nbsp;<a href='javascript:void(0);' class='btn-sm btn-danger'>Reject</a>".html_safe
+    when 'Accepted'
+    else 'Rejected'
+    end
+  end
+
   def format_subjects_for_dictamination(subjects)
     subjects.map(&:name).join(", ") if subjects.present?
   end
