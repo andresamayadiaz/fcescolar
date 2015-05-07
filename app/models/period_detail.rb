2fe0082c7bd875d95ba6916b31c60c28716433f6
Kevin <kevin@kevinhq.com>
2015-05-07 09:44:18 +0700

    Change logic for start_month: The list of start months of the year selected

--- app/models/period_detail.rb
+++ app/models/period_detail.rb
@@ -5,9 +5,10 @@ class PeriodDetail < ActiveRecord::Base
   validates :period, :initial_month, :end_month, :year, :presence => true
 
   def get_months
-    months = []
-    months << self.initial_month.strftime('%b')
-    months << self.end_month.strftime('%b')
+    selected_year = self.year.strftime('%Y')
+    selected_period_id = self.period_id
+    selected_period_details = PeriodDetail.where('period_id=? and extract(year  from year) = ?',selected_period_id,selected_year)
+    months =  selected_period_details.map{|spd| spd.initial_month.strftime('%b') }
     months
   end
 end
