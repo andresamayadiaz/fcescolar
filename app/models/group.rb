6df4790b4c6a06b167f866d875fc3eceb6a517e1
Kevin <kevin@kevinhq.com>
2015-05-12 21:15:02 +0700

    Fix index attributes for nested attributes to save group_details

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ActiveRecord::Base
   belongs_to :study_plan
   has_many :group_details
+
+  accepts_nested_attributes_for :group_details, :reject_if => :all_blank, :allow_destroy => true
 end
