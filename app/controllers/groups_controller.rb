66742b39321d527c214874ffbebf1e1573057a90
Kevin <kevin@kevinhq.com>
2015-05-26 16:21:33 +0700

    Close group functionality

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,4 +4,9 @@ class GroupsController < ApplicationController
   def block_it
   end
 
+  def close_it
+    group_detail = GroupDetail.find(params[:group_detail_id])
+    group_detail.update(:status=>'Closed')
+    redirect_to :back, notice: 'This group is set to closed now'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   resources :groups do
     collection do
       get 'block_it'
+      get 'close_it'
     end
   end
 
