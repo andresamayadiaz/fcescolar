75974bd432c3e7d7cd0a03e65ffc446cb4c83dbd
Kevin <kevin@kevinhq.com>
2015-06-04 11:10:35 +0700

    Add students list to view group

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -14,6 +14,7 @@ class PeopleController < ApplicationController
     @group_detail = GroupDetail.find(params[:group_detail_id])
     active_role = current_user.active_role
     @able_to_block = active_role=='super_administrator' or active_role=='franchise_director' ? true : false
+    @enrolled_students = @group_detail.enrolled_students
     render  :pdf => "Group: #{@group_detail.group.group_id}" if params[:format]=='pdf'
   end
 
