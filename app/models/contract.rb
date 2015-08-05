9ecd1a7075ebc098aba76ca79112dd3cfa8c6187
Kevin <kevin@kevinhq.com>
2015-08-05 16:45:54 +0700

    Validate due_date : should be present for contract

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -2,4 +2,5 @@ class Contract < ActiveRecord::Base
   belongs_to :contracts_template
   belongs_to :person
   validates :contracts_template, :presence => true
+  validates :due_date, :presence => true
 end
