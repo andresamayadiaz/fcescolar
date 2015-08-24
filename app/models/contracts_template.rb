81d9f7f3b25f06f3753b5de5ca80edc38d0570ca
Kevin <kevin@kevinhq.com>
2015-08-24 13:54:29 +0700

    Contracts Templates Replace Keywords

--- app/models/contracts_template.rb
+++ app/models/contracts_template.rb
@@ -2,19 +2,4 @@ class ContractsTemplate < ActiveRecord::Base
   resourcify
   belongs_to :franchise
   scope :active, -> { where(status: true) }
-
-  def self.replace_keywords(content,person,franchise)
-    content.gsub! '@@person_fullname@@', person.name
-    content.gsub! '@@person_fullname2@@', person.name
-    content.gsub! '@@person_id@@', person.id.to_s
-    content.gsub! '@@username@@', person.name
-    content.gsub! '@@franchise_name@@', franchise.name
-    content.gsub! '@@franchise_address@@', franchise.address
-    content.gsub! '@@date_now@@', Time.now.strftime('%Y-%m-%d')
-    content.gsub! '@@date_time@@', Time.now.strftime('%Y-%m-%d %H:%M:%S')
-    content.gsub! '@@person_street@@', person.person_living_address.street
-    content.gsub! '@@person_streetnumber1@@', person.person_living_address.num_ext
-    return content
-  end
-
 end
