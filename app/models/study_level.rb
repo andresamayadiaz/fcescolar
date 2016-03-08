935e6a4e7cf92dce55a95a7f4d4ae583c3ed67cb
celesteortiz <celeste@pilotoautomatico.mx>
2016-03-08 13:04:01 -0600

    StudyLevel Model => Add function self.get_sl_selected

--- app/models/study_level.rb
+++ app/models/study_level.rb
@@ -1,4 +1,17 @@
 class StudyLevel < ActiveRecord::Base
   resourcify
   belongs_to :franchise
+  has_and_belongs_to_many :subjects
+  
+  def self.get_sl_selected(ids)
+    selected_arr = []
+    ids.split(",").each do |i|
+      sl = self.find(i)
+      selected_arr << {:name=>sl.try(:name)}
+    end
+    if ids
+    selected_arr
+  end
+  end
+
 end
