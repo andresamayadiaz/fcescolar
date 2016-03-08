de1429d9dd0f2a234de64d4c0f9354fbb9ffe959
celesteortiz <celeste@pilotoautomatico.mx>
2016-03-08 13:03:34 -0600

    CurricularLine Model => Add function self.get_cl_selected

--- app/models/curricular_line.rb
+++ app/models/curricular_line.rb
@@ -1,5 +1,15 @@
 class CurricularLine < ActiveRecord::Base
   resourcify
   belongs_to :franchise
-  has_many :subjects, dependent: :destroy
+  has_and_belongs_to_many :subjects
+
+  def self.get_cl_selected(ids)
+    selected_arr = []
+    ids.split(",").each do |i|
+      cl = self.find(i)
+      selected_arr << {:name=>cl.try(:name)}
+    end if ids
+    selected_arr
+  end
+
 end
