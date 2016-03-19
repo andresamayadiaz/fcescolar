752eac4305b7a7d3a3ed521fa4ef42ab6a2fd504
celesteortiz <celeste@pilotoautomatico.mx>
2016-03-19 12:23:37 -0600

    StudyPlan => Agregué método notificar_caducidad_study_period

--- app/models/study_plan.rb
+++ app/models/study_plan.rb
@@ -16,6 +16,31 @@ class StudyPlan < ActiveRecord::Base
   validates :acuerdo, length: {maximum: 15, message: "Acuerdo debe contener máximo de 15 digitos"}
   validates :beca, :inclusion => { :in => 10..50, message: "El valor debe ser entre 10% y 50 %" }
 
+  def self.notificar_caducidad_study_period
+    study_plans=StudyPlan.where('status = ?', 1)
+    users_send_mail = User.find_by_id(1) # A quien se le enviará la notificación? Por rol o por user?
+
+    if study_plans.present?
+
+      study_plans.each do |sp|  # Ej. Entra StudyPlan ID: 1  Vence el: 2020-03-17 
+
+        fecha_aviso1 = sp.fecha_caducidad - 14.month # 2019-01-17
+        fecha_aviso2 = sp.fecha_caducidad - 13.month # 2019-02-17
+        fecha_aviso3 = sp.fecha_caducidad - 12.month # 2019-03-17
+        today= Date.today
+        #today= Date.parse('2019-01-17')
+
+        if today == fecha_aviso1 || today == fecha_aviso2 || today == fecha_aviso3
+          #logger.debug "-----ENVÍA PRIMER AVISO ------"
+          NotificationMailer.notify_study_plan_vence(users_send_mail,sp).deliver
+
+          #Si ya se llego la fecha de vencimiento donde se cambiará el status a vencido y cual será el id de status vencido?
+        end
+      end
+    end
+
+  end
+
   def unique_subject
     arr_subjects = self.study_plan_periods.map{|x| x.study_plan_subjects}.flatten.map(&:subject_id)
     errors.add(:base, "Duplicated subject on same study plan") if arr_subjects.uniq.length!=arr_subjects.length
