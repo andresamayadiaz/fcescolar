2eaaf3dd9c38208047302238641e00488e6f1d2d
celesteortiz <celeste@pilotoautomatico.mx>
2016-03-23 09:09:52 -0600

    schedule.rb=> Nueva tarea

--- config/schedule.rb
+++ config/schedule.rb
@@ -20,10 +20,14 @@
 # Learn more: http://github.com/javan/whenever
 
 set :output, "/tmp/fcescolar_background_jobs.log"
+ENV['RAILS_ENV'] = "development"
 
 every 1.day, :at => '12:05 am' do
     runner 'Contract.with_due_date_tomorrow'
 end
 every 1.day, :at => '12:10 am' do
     runner 'PersonalRecordFile.responsive_letter_with_due_date_yesterday_or_less'
+end
+every 1.month, :at => "start of the month at 01:00 am" do # Que día del mes?
+    runner 'StudyPlan.notificar_caducidad_study_plan'
 end
