c82639754bc3e89481d66834e93f84b6fc693e65
celesteortiz <celeste@pilotoautomatico.mx>
2016-03-23 09:31:43 -0600

    models/study_plan.rb => Nuevo metodo notificar_caducidad_study_plan

--- app/models/study_plan.rb
+++ app/models/study_plan.rb
@@ -16,27 +16,18 @@ class StudyPlan < ActiveRecord::Base
   validates :acuerdo, length: {maximum: 15, message: "Acuerdo debe contener máximo de 15 digitos"}
   validates :beca, :inclusion => { :in => 10..50, message: "El valor debe ser entre 10% y 50 %" }
 
-  def self.notificar_caducidad_study_period
-    study_plans=StudyPlan.where('status = ?', 1)
-    users_send_mail = User.find_by_id(1) # A quien se le enviará la notificación? Por rol o por user?
+  def self.notificar_caducidad_study_plan   
+    study_plans=StudyPlan.where('12 * (YEAR(fecha_caducidad) - YEAR(curdate())) + (MONTH(fecha_caducidad) - MONTH(curdate())) <= 12 AND status=1')
+
+    roles = [1, 5, 3] # Coordinador academico, jefe de control escolar, super administrador.
+    users = UsersRole.select('user_id').where('role_id'=> roles)
+    users = User.where('id'=>users)
+    user_emails = users.map { |user| user.email }
 
     if study_plans.present?
-
-      study_plans.each do |sp|  # Ej. Entra StudyPlan ID: 1  Vence el: 2020-03-17 
-
-        fecha_aviso1 = sp.fecha_caducidad - 14.month # 2019-01-17
-        fecha_aviso2 = sp.fecha_caducidad - 13.month # 2019-02-17
-        fecha_aviso3 = sp.fecha_caducidad - 12.month # 2019-03-17
-        today= Date.today
-        #today= Date.parse('2019-01-17')
-
-        if today == fecha_aviso1 || today == fecha_aviso2 || today == fecha_aviso3
-          #logger.debug "-----ENVÍA PRIMER AVISO ------"
-          NotificationMailer.notify_study_plan_vence(users_send_mail,sp).deliver
-
-          #Si ya se llego la fecha de vencimiento donde se cambiará el status a vencido y cual será el id de status vencido?
+       study_plans.each do |sp| 
+          NotificationMailer.notify_study_plan_vence(user_emails,sp).deliver
         end
-      end
     end
 
   end
